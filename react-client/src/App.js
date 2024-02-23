f9d76e888c406bc4b93b596230200fdee1d6299d
Nobphadon Hankittikanjana <nopz3456z@gmail.com>
2024-02-23 22:50:19 +0700

    change to master la

---
+++ react-client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Auth } from "./pages/auth";
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+       <Routes>
+       <Route path='/' element={<Home/>}> </Route>
+           <Route path='/auth' element={<Auth/>}> </Route>
+       </Routes>
+     </Router>
+    </div>
+   );
+}
+
+export default App;
