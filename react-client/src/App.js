c84d30a960ed6e3a7cdb0bb2aaf95c9589e85f9f
KaEnds <kenza.ke15@gmail.com>
2024-02-23 23:16:20 +0700

    i love you

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Auth } from "./pages/auth";
+import { Home } from "./pages/home";
 
 function App() {
   return (
--- react-client/src/pages/home.js
+++ react-client/src/pages/home.js
@@ -4,6 +4,8 @@ export const Home = () => {
     return(
         <div>
             <h1>Home</h1>
+            <p>i love you to much</p>
+
         </div>
 
     );
