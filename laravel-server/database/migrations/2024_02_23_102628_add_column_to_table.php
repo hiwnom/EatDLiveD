cf8436bb07eed78b9066b8d4d8c05fac1c6ea340
Nobphadon Hankittikanjana <nopz3456z@gmail.com>
2024-03-01 16:54:44 +0700

    edit users db and typos#2

--- laravel-server/database/migrations/2024_02_23_102628_add_column_to_table.php
+++ laravel-server/database/migrations/2024_02_23_102628_add_column_to_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->double('heigth')->after('password');
-            $table->double('weigth')->after('password');
+            $table->double('height')->after('password');
+            $table->double('weight')->after('password');
         });
     }
 
